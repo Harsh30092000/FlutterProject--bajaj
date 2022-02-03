96f180531fd4badb7867e9cceff5719fab1b9307
Harsh30092000 <harshith30092000@gmail.com>
2022-02-03 12:18:50 +0530

    Added Body and Floating action button

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,22 @@ void main() {
       MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text('App Bar'),
+            title: Text(
+                'App Bar',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color(0xff7605a6)
+              ),
+            ),
+          ),
+          body: Center(
+            child: Text(
+              "Hello World!"
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Text("Click"),
+            onPressed: (){},
           ),
         ),
       ));
